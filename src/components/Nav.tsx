d8a9d2a58137a5dbe1f34e1a1ebfd220dbff8ba2
leodvincci <lpenrose@depaul.edu>
2025-12-30 14:06:23 -0600

    Enhance Nav component UI and styling: - Add menu items and login button to the navigation bar. - Update styles for improved layout, spacing, and hover effects.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,15 @@
 function Nav(){
     return(
         <nav>
-            <p>Bibby</p>
+            <p id={"bibby-logo"}>Bibby</p>
+            <ul id={"nav-items"}>
+                <li>Search</li>
+                <li>Library Management</li>
+                <li>Book Cart</li>
+                <li>Booklists</li>
+            </ul>
+
+            <p id={"sign-in-btn"} className={"btn"}>Login</p>
         </nav>
     )
 }
