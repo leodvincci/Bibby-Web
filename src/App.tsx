e651acce54087691cdbcff9d6ee31d5d32397f7b
leodvincci <lpenrose@depaul.edu>
2025-12-30 13:17:48 -0600

    Add Nav component and update App to render it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import {createRoot} from 'react-dom/client'
+import {Nav} from "./components/Nav.tsx";
 
 const container = document.getElementById("app")
 if (!container) throw new Error('Root element with id `app` not found')
 
 createRoot(container).render(
     <div>
-        <h1>Hello Bibby</h1>
+        <Nav/>
     </div>
 )
 
---
+++ src/components/Nav.tsx
@@ -0,0 +1,13 @@
+
+
+function Nav(){
+    return(
+        <nav>
+            <h1>Hello Nav</h1>
+        </nav>
+    )
+}
+
+
+
+export {Nav}
