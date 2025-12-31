ecfe1ed1f9f0dc350aeb7480d7b8dd5e07d6cbb6
leodvincci <lpenrose@depaul.edu>
2025-12-31 01:52:49 -0600

    Refactor component structure and update styles: - Move `SearchPage` to a dedicated `Search` folder. - Relocate `Nav` to its own folder, update imports accordingly. - Add accessible input field to `SearchPage`. - Style new `search-input` field in `style.css`. - Rename and reposition `bibbyMark.png`. - Add new `search_bookIcon.png`.

--- src/components/Search/SearchPage.tsx
+++ src/components/Search/SearchPage.tsx
@@ -2,6 +2,8 @@ function SearchPage(){
     return(
         <section className="search-page">
             <p className="page-title">Book Search</p>
+            <label htmlFor={"search-input"}  aria-label={"book search input"}> </label>
+            <input id={"search-input"} type={"text"} placeholder={"e.g. 9780517542095"} />
         </section>
         )
 }
