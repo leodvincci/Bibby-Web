4b7484b8f28887001c77fea3b4e3338830431a3d
leodvincci <lpenrose@depaul.edu>
2025-12-31 03:36:06 -0600

    Refactor SearchContainer component and improve styles: - Add `search-by-radio-options` and restructure radio button layout. - Introduce new styles for `.search-form`, `.search-by-label`, and `.search-by-radio-options` in `style.css`. - Update `page-title` font weight for better readability.

--- src/components/Search/SearchContainer.tsx
+++ src/components/Search/SearchContainer.tsx
@@ -3,24 +3,36 @@ function SearchContainer(){
         <div className={"search-container"}>
 
 
-            <form action={()=>{console.log("Submitted Form")}}>
+            <form className={'search-form'} action={()=>{console.log("Submitted Form")}}>
 
                 <div>
                     <label htmlFor={"search-input"}  aria-label={"book search input"}></label>
                     <input id={"search-input"} type={"text"} placeholder={"e.g. 9780517542095"} />
                     <button  type={"submit"} className={"book-search-icon search-btn"} style={{ border: "none", background: "none", padding: 0,height:".05px",width:".05px", position:"static"}}> <img className={"book-search-icon"} src={"../../public/search_bookIcon.png"} alt={"book icon"}/> </button>
-
                 </div>
 
 
-                <label htmlFor={"isbn-radio"}>Search By ISBN</label>
-                <input id="isbn-radio" type={"radio"} />
+                <div className={"search-by-radio-options"}>
 
-                <label htmlFor={"isbn-radio"}>Search By Title</label>
-                <input id={"title-radio"} type={"radio"} />
+                    <section>
+                        <input defaultChecked={true} id="isbn-radio" type={"radio"} name={"search-by"}/>
+                        <label className={"search-by-label"} htmlFor={"isbn-radio"}>Search by ISBN</label>
+                    </section>
 
-                <label htmlFor={"isbn-radio"}>Search By Author</label>
-                <input id={"author-radio"} type={"radio"} />
+
+                    <section>
+                        <input id={"title-radio"} type={"radio"} name={"search-by"}/>
+                        <label className={"search-by-label"} htmlFor={"title-radio"}>Search by Title</label>
+                    </section>
+
+
+                    <section>
+                        <input id={"author-radio"} type={"radio"} name={"search-by"} />
+                        <label className={"search-by-label"} htmlFor={"author-radio"}>Search by Author</label>
+                    </section>
+
+                </div>
+
             </form>
 
         </div>
