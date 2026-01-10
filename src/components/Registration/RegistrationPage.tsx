3893e7d0c62fe866ec69366849d210b4ad475ff9
leodvincci <lpenrose@depaul.edu>
2026-01-10 00:53:38 -0600

    Add registration page layout and styles with logo and background image

--- src/components/Registration/RegistrationPage.tsx
+++ src/components/Registration/RegistrationPage.tsx
@@ -1,9 +1,13 @@
+import BibbyLogo from "../../../public/bibbyLogo.png";
 function RegistrationPage(){
 
 
     return(
         <div id={"registration-page"} className="reg-background">
-            <h1>Registration</h1>
+            <div className="registration-container">
+                <img className="bibby-logo" src={BibbyLogo} alt="book background"/>
+                <h3 className="call-to-action">Add your first book.</h3>
+            </div>
         </div>
     )
 
