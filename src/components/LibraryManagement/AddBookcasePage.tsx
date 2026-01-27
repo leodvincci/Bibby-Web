e0a5b7926e29f8a5cfdf5567c2d480bc945ae608
leodvincci <lpenrose@depaul.edu>
2026-01-26 18:33:57 -0600

    Enhance AddBookcasePage layout with navigation and instructional text

--- src/components/LibraryManagement/AddBookcasePage.tsx
+++ src/components/LibraryManagement/AddBookcasePage.tsx
@@ -1,9 +1,17 @@
+import { Nav } from "../Nav/Nav";
 
 function AddBookcasePage() {
-  return (
-    <div>
-      <h1>Add New Bookcase</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Nav />
+            <h1 className="blu fw-500" >Add a new bookcase to your library</h1>
+            <p>Configure your bookcase details and capacity settings below.</p>
+
+            <form className="add-bookcase-form">
+
+
+            </form>
+        </div>
+    );
 }
 export { AddBookcasePage }
