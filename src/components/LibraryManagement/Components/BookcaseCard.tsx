cf9ca015090a0c2aa9d38abf3eccb21122b3cd59
leodvincci <lpenrose@depaul.edu>
2026-01-28 20:38:50 -0600

    Add BookcaseCard component and update ViewBookcasesPage layout

---
+++ src/components/LibraryManagement/Components/BookcaseCard.tsx
@@ -0,0 +1,15 @@
+
+function BookcaseCard() {
+    return (
+        <div className=" border bookcase-card w-450p h-350p m-5 p-4">
+            <h2>Location</h2>
+            <p>Zone</p>
+            <p>Identifier</p>
+            <p>Capacity</p>
+            <p>Number of Shelves: 5</p>
+            <p>Last Placed: last placed book title</p>
+        </div>
+    );
+}
+
+export { BookcaseCard };
--- src/components/LibraryManagement/ViewBookcasesPage.tsx
+++ src/components/LibraryManagement/ViewBookcasesPage.tsx
@@ -1,11 +1,21 @@
-import {Nav} from "../Nav/Nav";
+import { Nav } from "../Nav/Nav";
+import { Button } from "@radix-ui/themes";
+import { BookcaseCard } from "./Components//BookcaseCard";
 
 function ViewBookcasesPage() {
     return (
         <div>
-            <Nav/>
-            <h1>View Bookcases</h1>
-            {/* Additional content and components for viewing bookcases would go here */}
+            <Nav />
+            <h1 className="blu">Bookcases</h1>
+            <p>Pick a bookcase to see what's on each shelf.</p>
+            <Button color="cyan">+ New bookcase</Button>
+
+            <section className="flex" id="bookcasecard-section">
+                <BookcaseCard />
+                <BookcaseCard />
+                <BookcaseCard />
+            </section>
+
         </div>
     );
 }
